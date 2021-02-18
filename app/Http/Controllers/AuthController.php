1b7a21ebe1e28bbae81b3d98131e1c1e1dd8b7e0
Flórián Demecs <demecsflorian@gmail.com>
2021-02-18 23:56:31 +0100

    Token validálásra alapozás

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\App;
+use GuzzleHttp;
 
 class AuthController extends Controller
 {
@@ -19,24 +22,20 @@ class AuthController extends Controller
     public function callback(){
         $oauth = Socialite::driver('azure')->user();
 
-        echo $oauth->getName();
-        echo $oauth->getId();
-        echo $oauth->getEmail();
-        echo time();
-        dd(Carbon::createFromTimestamp($oauth->accessTokenResponseBody['expires_on'], 'Europe/Budapest')->toDateTimeString());
+        dd($oauth);
 
+        if(User::where('email', $oauth->email->count() == 1){
 
-        /*if(User::where('email', $oauth->getEmail())->count() == 1){
+            $user = User::where('email', $oauth->email->first());
 
-            $user = User::where('email', $oauth->getEmail())->first();
             if($user->name == ""){
-                $user->name = $ouath->getName();
-                $user->accessToken = $oauth->getToken();
-                $user->refreshToken = $oauth->getRefreshToken();
-                $user->tokenExpires = $oauth->getExpires();
+                $user->name = $ouath->name;
+                $user->accessToken = $oauth->accessTokenResponseBody['access_token'];
+                $user->refreshToken = $oauth->accessTokenResponseBody['refresh_token'];
+                $user->tokenExpires = Carbon::createFromTimestamp($oauth->accessTokenResponseBody['expires_on'], 'Europe/Budapest')->toDateTimeString();
+
+                $user->save();
             }
-            $user->token = $ouath->user['token'];
-            $user->save();
-        }*/
+        }
     }
 }
---
+++ app/Http/Middleware/TokenValidate.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TokenValidate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check() && ($user = Auth::user())->tokenExpires <= time() + 300) {
+            // Check User and Token is expired (or very close to it /5 min/)
+
+            $client = new GuzzleHttp\Client();
+            try{
+                $json = $client->request('POST', 'https://login.microsoftonline.com/common/oauth2/token', [
+                    'form_params' => [
+                        'grant_type' => 'refresh_token',
+                        'client_id' => config('services.azure.client_id'),
+                        'client_secret' => config('services.azure.client_secret'),
+                        'refresh_token' => $user->refreshToken,
+                    ]
+                ]);
+
+                if ($json->getStatusCode() != 200) throw new Exception;
+
+                $response = json_decode($json->getBody(), true);
+
+                // Store the new values
+                $user->accessToken = $response->access_token;
+                $user->refreshToken = $response->refresh_token;
+                $user->tokenExpires = Carbon::createFromTimestamp($response->expires_on, 'Europe/Budapest')->toDateTimeString();
+
+                $user->save();
+
+            }
+
+            catch(Exception $e) {
+                Auth::logout();
+            }
+        }
+
+        return $next($request);
+    }
+}
