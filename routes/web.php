f309a1619cdfcd9fba84fcce4de23d2be341a102
Flórián Demecs <demecsflorian@gmail.com>
2021-02-28 22:56:39 +0100

    Routing updated for changes  - AdminController  - grouping added

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
 use \App\Http\Controllers\StudentController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,29 +20,38 @@ use \App\Http\Controllers\StudentController;
 
 Route::get('/', function(){
     return redirect('/schedule');
-});
+})->name('home');
 
 Route::get('/login', [AuthController::class, 'oauth'])->name('login');
 Route::get('/login/local', [AuthController::class, 'login'])->name('login.local');
 Route::post('/login/local', [AuthController::class, 'loginPost']);
-Route::get('/logout', function (Request $request){
-    // TODO: Maybe should clear token values?
-    Auth::logout();
-    $request->session()->invalidate();
-    $request->session()->regenerateToken();
-    return 'KILÉPVE!';
-})->name('logout');
 Route::get('/callback', [AuthController::class, 'callback'])->name('callback');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/schedule', [StudentController::class, 'login'])->name('schedule');
 Route::post('/schedule', [StudentController::class, 'index'])->name('schedule.index');
 
 
-Route::get('/dashboard', function (){
-    return view('private.index');
-})->name('dashboard')->middleware('auth');
-
 Route::get('/school', [StudentController::class, 'getSchoolData'])->name('omschools')->middleware('auth');
 
-Route::get('/import', [StudentController::class, 'importView'])->middleware('auth');
-Route::post('/import', [StudentController::class, 'import'])->name('import')->middleware('auth');
+Route::prefix('dashboard')->name('dashboard.')->middleware('auth')->group(function () {
+
+    Route::get('/', [AdminController::class, 'dashboardIndex'])->name('index');
+
+    Route::prefix('import')->name('import.')->group(function () {
+        Route::get('/', [AdminController::class, 'importView'])->name('get');
+        Route::post('/', [AdminController::class, 'import'])->name('post');
+    });
+
+    Route::prefix('student')->name('student.')->group(function () {
+
+        Route::prefix('oralexam')->name('oralexam.')->group(function () {
+            Route::get('/', [AdminController::class, 'getStudentOralExamInfoIndex'])->name('index');
+            Route::post('/', [AdminController::class, 'getStudentOralExamInfoRequest'])->name('request');
+            Route::get('/{id}', [AdminController::class, 'getStudentOralExamInfo'])->name('info');
+        });
+    });
+});
+
+
+
