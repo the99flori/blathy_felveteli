5ee70984746c3ff6ab2b68c5c9085bdbfe8ea0ce
Flórián Demecs <demecsflorian@gmail.com>
2021-02-28 22:53:56 +0100

    AdminController added  - getStudentOralExamInfo  - Dashboard stuff some function moved from StudentController to there  - getSchoolData  - import

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,13 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\getScheduleRequest;
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
-
-use Maatwebsite\Excel\Facades\Excel;
-use App\Imports\StudentsImport;
 
 use App\Models\Student;
-use App\Models\primarySchool;
 use App\Models\StudentLog;
 
 class StudentController extends Controller
@@ -44,49 +39,6 @@ class StudentController extends Controller
         return view('schedule.index', [
             'student' => $student,
         ]);
-    }
-
-    public function getSchoolData(){
-        function get_string_between($string, $start, $end){
-            $string = ' ' . $string;
-            $ini = strpos($string, $start);
-            if ($ini == 0) return '';
-            $ini += strlen($start);
-            $len = strpos($string, $end, $ini) - $ini;
-            return substr($string, $ini, $len);
-        }
-
-        $client = new Client();
-
-        foreach (Student::select('primaryOM')->get() as $om){
-            $response = $client->request('GET', 'https://www.oktatas.hu/kozneveles/intezmenykereso/koznevelesi_intezmenykereso/!DARI_Intezmenykereso/oh.php?id=kir_int_mod&param='.$om->primaryOM);
-
-            $parsed = get_string_between($response->getBody()->getContents(), 'A(z)', ') köznevelési');
-            $pieces = explode("(", $parsed);
-
-            primarySchool::firstOrCreate([
-                'om' => $om->primaryOM,
-            ],
-                [
-                    'name' => trim($pieces[0]),
-                    'address' => trim($pieces[1]),
-                ]);
-
-        }
-
-
-    }
-
-    public function importView()
-    {
-        return view('import');
-    }
-
-    public function import()
-    {
-        Excel::import(new StudentsImport,request()->file('file'));
-
-        return redirect()->back();
     }
 
 }
